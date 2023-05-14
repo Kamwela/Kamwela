21adc91aa6d7253fb07b40bc9fd5fec7d25fe0b4
Kamwela <130525297+Kamwela@users.noreply.github.com>
2023-05-14 03:31:22 -0600

    Delete Grocery/Grocery directory

--- Grocery/Grocery/src/main/java/com/Grocery_store/Grocery/repository/GroceryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Grocery_store.Grocery.repository;
-
-import com.Grocery_store.Grocery.entity.GroceryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroceryRepository extends JpaRepository<GroceryEntity, Long> {
-
-}
