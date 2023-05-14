f3a2f33971d16444ff93c1889805ff1cc44edac9
Kamwela <130525297+Kamwela@users.noreply.github.com>
2023-05-14 03:32:42 -0600

    Add files via upload

---
+++ Grocery/Grocery/src/main/java/com/Grocery_store/Grocery/service/GroceryServiceImpl.java
@@ -0,0 +1,50 @@
+package com.Grocery_store.Grocery.service;
+
+import com.Grocery_store.Grocery.entity.GroceryEntity;
+import com.Grocery_store.Grocery.repository.GroceryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class GroceryServiceImpl implements GroceryService {
+    private GroceryRepository groceryRepository;
+
+    @Override
+    public GroceryEntity createGrocery(GroceryEntity groceryEntity) {
+
+        return groceryRepository.save(groceryEntity);
+    }
+
+    @Override
+    public GroceryEntity getGroceryById(Long groceryId) {
+        Optional<GroceryEntity> optionalGroceryEntity = groceryRepository.findById(groceryId);
+        return optionalGroceryEntity.get();
+    }
+
+    @Override
+    public List<GroceryEntity> getAllGrocery() {
+        return groceryRepository.findAll();
+    }
+
+    @Override
+    public GroceryEntity updateGrocery(GroceryEntity groceryEntity) {
+        GroceryEntity existingGrocery = groceryRepository.findById(groceryEntity.getId()).get();
+        existingGrocery.setProductName(groceryEntity.getProductName());
+        existingGrocery.setArriveDate(groceryEntity.getArriveDate());
+        existingGrocery.setProductCost(groceryEntity.getProductCost());
+        existingGrocery.setPaymentMode(groceryEntity.getPaymentMode());
+        existingGrocery.setStatus(groceryEntity.getStatus());
+        GroceryEntity updatedGrocery = groceryRepository.save(existingGrocery);
+        return updatedGrocery;
+    }
+
+    @Override
+    public void deleteGrocery(Long groceryId) {
+        groceryRepository.deleteById(groceryId);
+
+    }
+}
